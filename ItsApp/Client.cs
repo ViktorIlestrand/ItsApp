8359854dfa71c05e3b950a6030eb127f0ab8799e
ScrumMaster <Administrator@ACADEMY-6211VVS>
2016-11-15 16:39:54 +0100

    Lite mer ändringar

--- ItsApp/Client.cs
+++ ItsApp/Client.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
@@ -12,12 +13,39 @@ namespace ItsApp
     {
         private TcpClient client;
 
+      
+
         public void Start()
         {
             Console.WriteLine("Skriv in din IP-adress, Benke");
             string input = Console.ReadLine();
             client = new TcpClient(input, 9965);
             
+               
+        }
+
+        public void Sender()
+        {
+            string message = "";
+
+            try
+            {
+                NetworkStream n = client.GetStream();
+
+                while (!message.ToLower().Equals("quit"))
+                {
+                    message = Console.ReadLine();
+                    BinaryWriter writer = new BinaryWriter(n);
+                    writer.Write(message);
+                    writer.Flush();
+                }
+
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
